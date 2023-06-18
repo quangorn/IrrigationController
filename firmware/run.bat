45223df1f0d45f58cc15028a5daf62f5394c5c30
karamyshev <karamyshev@protei.ru>
2023-06-18 18:14:27 +0300

    В run.bat добавлена загрузка в eeprom

--- firmware/run.bat
+++ firmware/run.bat
@@ -1,3 +1,4 @@
 avr-objcopy.exe -j .text -j .data -O ihex %1 %1.hex
+avr-objcopy -j .eeprom --set-section-flags .eeprom="alloc,load" --change-section-lma .eeprom=0 -O ihex %1 %1.eep
 avr-size.exe -C --mcu=attiny13a %1
-avrdude.exe -p t13 -c usbasp -P usb -U flash:w:%1.hex:i
+avrdude.exe -p t13 -c usbasp -P usb -U flash:w:%1.hex:i -U eeprom:w:%1.eep:i
